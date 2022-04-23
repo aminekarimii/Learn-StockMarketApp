355edd2fdd3b8426e43e08c7010567159841fd22
Amine karimi <aminekarimi1998@gmail.com>
2022-04-23 02:31:43 +0000

    Extend Room database in AppDatabase.kt

--- app/src/main/java/com/plcoding/stockmarketapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/local/AppDatabase.kt
@@ -1,12 +1,13 @@
 package com.plcoding.stockmarketapp.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [CompanyEntity::class],
     version = 1
 )
 
-abstract class AppDatabase {
+abstract class AppDatabase : RoomDatabase() {
     abstract val stockDao: StockDao
 }
