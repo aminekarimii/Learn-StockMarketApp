23dc57132d92c06625cc85b3cad2b99db95314ad
Amine karimi <aminekarimi1998@gmail.com>
2022-04-23 02:32:09 +0000

    Create companies listing UI

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/companylist/CompanyItem.kt
@@ -0,0 +1,58 @@
+package com.plcoding.stockmarketapp.presentation.companylist
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plcoding.stockmarketapp.domain.model.Company
+
+@Composable
+fun CompanyItem(
+    company: Company,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Row(
+                modifier = modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = company.name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colors.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = company.exchange,
+                    fontWeight = FontWeight.Light,
+                    color = MaterialTheme.colors.onBackground,
+                )
+            }
+
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Text(
+                text = "(${company.symbol})",
+                fontWeight = FontWeight.Light,
+                color = MaterialTheme.colors.onBackground,
+            )
+        }
+
+    }
+
+}
